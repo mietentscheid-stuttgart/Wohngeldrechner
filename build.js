7950df5f5a16341377ce58cf71b01c68be7d2986
moormaster <moormaster@gmx.net>
2019-11-24 15:36:07 +0100

    removed bower

--- build.js
+++ build.js
@@ -5,6 +5,7 @@ var layouts = require('metalsmith-layouts');
 var pug = require('metalsmith-pug');
 
 var fs = require('fs');
+var path = require('path');
 
 module.exports.build = function () {
 	return new Promise( function(resolve, reject) {
@@ -20,8 +21,53 @@ module.exports.build = function () {
 			}))
 			.build(function(err, files) {
 				if (err) { reject(err); throw err; }
-		
-				fs.symlink('.', './build/wohnberechtigungsschein-rechner', function(err) { if (err) { throw err; } });
+
+				// make wp-post.html usable in github pages, too
+				fs.symlink('.', path.join(config.buildRoot, 'wohnberechtigungsschein-rechner'), function(err) { if (err) { throw err; } });
+
+				// copy assets into build dir
+				[
+					{ source: path.join('node_modules', 'jquery', 'dist'),	target: path.join('libs', 'jquery'),			name: 'jquery.min.js' },
+
+					{ source: path.join('node_modules', 'jquery-ui-dist'),	target: path.join('libs', 'jquery-ui'),			name: 'jquery-ui.min.js' },
+					{ source: path.join('node_modules', 'jquery-ui-dist'),	target: path.join('libs', 'jquery-ui'),			name: 'jquery-ui.min.css' },
+
+					{ source: path.join('node_modules', 'jqplot', 'src'),			target: path.join('libs', 'jquery-jqplot'),		name: 'jquery.jqplot.js' },
+					{ source: path.join('node_modules', 'jqplot', 'src'),			target: path.join('libs', 'jquery-jqplot'),		name: 'jqplot.core.js' },
+					{ source: path.join('node_modules', 'jqplot', 'src'),			target: path.join('libs', 'jquery-jqplot'),		name: 'jqplot.linearTickGenerator.js' },
+					{ source: path.join('node_modules', 'jqplot', 'src'),			target: path.join('libs', 'jquery-jqplot'),		name: 'jqplot.linearAxisRenderer.js' },
+					{ source: path.join('node_modules', 'jqplot', 'src'),			target: path.join('libs', 'jquery-jqplot'),		name: 'jqplot.axisTickRenderer.js' },
+					{ source: path.join('node_modules', 'jqplot', 'src'),			target: path.join('libs', 'jquery-jqplot'),		name: 'jqplot.axisLabelRenderer.js' },
+					{ source: path.join('node_modules', 'jqplot', 'src'),			target: path.join('libs', 'jquery-jqplot'),		name: 'jqplot.tableLegendRenderer.js' },
+					{ source: path.join('node_modules', 'jqplot', 'src'),			target: path.join('libs', 'jquery-jqplot'),		name: 'jqplot.lineRenderer.js' },
+					{ source: path.join('node_modules', 'jqplot', 'src'),			target: path.join('libs', 'jquery-jqplot'),		name: 'jqplot.markerRenderer.js' },
+					{ source: path.join('node_modules', 'jqplot', 'src'),			target: path.join('libs', 'jquery-jqplot'),		name: 'jqplot.divTitleRenderer.js' },
+					{ source: path.join('node_modules', 'jqplot', 'src'),			target: path.join('libs', 'jquery-jqplot'),		name: 'jqplot.canvasGridRenderer.js' },
+					{ source: path.join('node_modules', 'jqplot', 'src'),			target: path.join('libs', 'jquery-jqplot'),		name: 'jqplot.linePattern.js' },
+					{ source: path.join('node_modules', 'jqplot', 'src'),			target: path.join('libs', 'jquery-jqplot'),		name: 'jqplot.shadowRenderer.js' },
+					{ source: path.join('node_modules', 'jqplot', 'src'),			target: path.join('libs', 'jquery-jqplot'),		name: 'jqplot.shapeRenderer.js' },
+					{ source: path.join('node_modules', 'jqplot', 'src'),			target: path.join('libs', 'jquery-jqplot'),		name: 'jqplot.sprintf.js' },
+					{ source: path.join('node_modules', 'jqplot', 'src'),			target: path.join('libs', 'jquery-jqplot'),		name: 'jsdate.js' },
+					{ source: path.join('node_modules', 'jqplot', 'src'),			target: path.join('libs', 'jquery-jqplot'),		name: 'jqplot.themeEngine.js' },
+					{ source: path.join('node_modules', 'jqplot', 'src'),			target: path.join('libs', 'jquery-jqplot'),		name: 'jqplot.toImage.js' },
+					{ source: path.join('node_modules', 'jqplot', 'src'),			target: path.join('libs', 'jquery-jqplot'),		name: 'jqplot.effects.core.js' },
+					{ source: path.join('node_modules', 'jqplot', 'src'),			target: path.join('libs', 'jquery-jqplot'),		name: 'jqplot.effects.blind.js' },
+					{ source: path.join('node_modules', 'jqplot', 'src'),			target: path.join('libs', 'jquery-jqplot'),		name: 'jquery.jqplot.css' },
+					{ source: path.join('node_modules', 'jqplot', 'src', 'plugins'),	target: path.join('libs', 'jquery-jqplot', 'plugins'),	name: 'jqplot.barRenderer.js' },
+					{ source: path.join('node_modules', 'jqplot', 'src', 'plugins'),	target: path.join('libs', 'jquery-jqplot', 'plugins'),	name: 'jqplot.categoryAxisRenderer.js' },
+					{ source: path.join('node_modules', 'jqplot', 'src', 'plugins'),	target: path.join('libs', 'jquery-jqplot', 'plugins'),	name: 'jqplot.pointLabels.js' }
+				].forEach( (asset) => {
+					fs.mkdirSync(
+						path.join(config.buildRoot, asset.target),
+						{ recursive: true }
+					);
+					fs.copyFileSync(
+						path.join(asset.source, asset.name), 
+						path.join(config.buildRoot, asset.target, asset.name)
+					);
+				} );
+
+				// notify promise to be resolved
 				resolve();
 			});
 	} );
--- src/insertcss.js
+++ src/insertcss.js
@@ -1,5 +1,5 @@
-document.writeln('<link rel="stylesheet" href="/wohnberechtigungsschein-rechner/bower_components/jquery-ui/themes/base/jquery-ui.min.css"/>\n\
-<link rel="stylesheet" href="/wohnberechtigungsschein-rechner/bower_components/jqplot-bower/dist/jquery.jqplot.min.css"/>\n\
+document.writeln('<link rel="stylesheet" href="/wohnberechtigungsschein-rechner/libs/jquery-ui/jquery-ui.min.css"/>\n\
+<link rel="stylesheet" href="/wohnberechtigungsschein-rechner/libs/jquery-jqplot/jquery.jqplot.css"/>\n\
 \n\
 <link rel="stylesheet" href="/wohnberechtigungsschein-rechner/wohnberechtigung-ui.css"/>');
 
