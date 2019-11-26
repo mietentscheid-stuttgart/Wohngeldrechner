d2e930df4532ed2d5d357784eae8fdd1d38b89d9
Andre Herbst <moormaster@gmx.net>
2019-11-26 07:00:00 +0100

    added port parameter to server.js

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ var express = require('express');
 var app = express();
 
 if (require.main === module) {
+	var port = config.http.port;
+	if (process.argv.length > 2)
+		port = parseInt(process.argv[2]);
+
 	app.use(express.static('build'))
-	app.listen(config.http.port, function () {
-		console.log('HTTP server listening on port ' + config.http.port);
+	app.listen(port, function () {
+		console.log('HTTP server listening on port ' + port);
 	});
 }
